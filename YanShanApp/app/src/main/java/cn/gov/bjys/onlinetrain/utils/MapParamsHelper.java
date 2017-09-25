eba97112aff31f3b54325d94b4a26bd72b8625fc
zhoudodo <635547767@qq.com>
2017-09-25 18:26:12 +0800

    111

--- YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/utils/MapParamsHelper.java
+++ YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/utils/MapParamsHelper.java
@@ -7,10 +7,8 @@ import java.util.Map;
  * Created by dodozhou on 2017/9/25.
  */
 public class MapParamsHelper {
-
     public static Map<String, Object> getBaseParamsMap() {
         Map<String, Object> params = new HashMap<String, Object>();
         return params;
     }
-
 }
