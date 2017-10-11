572d53fa51b39cb62169859fb4fe3c0abf3f1779
zhoudodo <635547767@qq.com>
2017-10-12 01:04:54 +0800

    add class

--- YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/act/ClassActivity.java
+++ YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/act/ClassActivity.java
@@ -1,20 +1,66 @@
 package cn.gov.bjys.onlinetrain.act;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.FrameLayout;
+
 import com.ycl.framework.base.FrameActivity;
 
+import java.io.BufferedReader;
+
+import butterknife.Bind;
 import cn.gov.bjys.onlinetrain.R;
+import cn.gov.bjys.onlinetrain.fragment.ClassFragment.ClassGridFragment;
+import cn.gov.bjys.onlinetrain.fragment.ClassFragment.ClassLinearFragment;
 
 /**
  * Created by Administrator on 2017/10/10 0010.
  */
 public class ClassActivity extends FrameActivity {
 
+    public static final String CLASS_TYPE = "TYPE";
+    public static final int CLASS_GRID_TAG = 2;
+    public static final int CLASS_LINEAR_TAG = 1;
+    @Bind(R.id.frame_layout)
+    FrameLayout mFrameLayout;
+    private Fragment[] mFragments;
+
     @Override
     protected void setRootView() {
-            setContentView(R.layout.activity_class_layout);
+        setContentView(R.layout.activity_class_layout);
     }
 
 
+    @Override
+    public void initViews() {
+        super.initViews();
+        mFragments = new Fragment[]{
+                getClassGridFragment(),
+                getClassLinearFragment()
+        };
 
+    }
 
+    private void showFragment() {
+        FragmentTransaction fts = getSupportFragmentManager().beginTransaction();
+        Intent recIntent = getIntent();
+        int r = recIntent.getIntExtra(CLASS_TYPE, CLASS_LINEAR_TAG);
+        switch (r) {
+            case CLASS_GRID_TAG:
+                fts.add(R.id.frame_layout, mFragments[0]).show(mFragments[0]).commit();
+                break;
+            case CLASS_LINEAR_TAG:
+                fts.add(R.id.frame_layout, mFragments[1]).show(mFragments[1]).commit();
+                break;
+        }
+    }
+
+    public ClassGridFragment getClassGridFragment() {
+        return ClassGridFragment.newInstance();
+    }
+
+    public ClassLinearFragment getClassLinearFragment() {
+        return ClassLinearFragment.newInstance();
+    }
 }
--- YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/fragment/ClassFragment/ClassGridFragment.java
+++ YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/fragment/ClassFragment/ClassGridFragment.java
@@ -1,31 +1,74 @@
 package cn.gov.bjys.onlinetrain.fragment.ClassFragment;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.ycl.framework.base.FrameFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
 import cn.gov.bjys.onlinetrain.R;
 import cn.gov.bjys.onlinetrain.adapter.DooClassRecycleAdapter;
+import cn.gov.bjys.onlinetrain.adapter.DooSimpleMultiAdapter;
+import cn.gov.bjys.onlinetrain.bean.YSClassBean;
 
 /**
  * Created by Administrator on 2017/10/10 0010.
  */
 public class ClassGridFragment extends FrameFragment {
 
+    public static ClassGridFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        ClassGridFragment fragment = new ClassGridFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
     @Override
     protected View inflaterView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
         return inflater.inflate(R.layout.fragment_linear_layout, container, false);
     }
 
-    private RecyclerView mRecyclerView;
+    @Bind(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+
+    @Bind(R.id.rv)
+    RecyclerView mRecyclerView;
 
     @Override
     protected void initViews() {
         super.initViews();
-
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        List<YSClassBean> data = prepareTestData();
+        final DooSimpleMultiAdapter multipleItemAdapter = new DooSimpleMultiAdapter(data);
+        final GridLayoutManager manager = new GridLayoutManager(getContext(), 2);
+        mRecyclerView.setLayoutManager(manager);
+        multipleItemAdapter.setSpanSizeLookup(new BaseQuickAdapter.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(GridLayoutManager gridLayoutManager, int position) {
+                return data.get(position).getSpanSize();
+            }
+        });
+        mRecyclerView.setAdapter(multipleItemAdapter);
     }
+
+    private List<YSClassBean> prepareTestData(){
+        List<YSClassBean> datas = new ArrayList<>();
+        for(int i=0;i<50;i++){
+
+        }
+        return datas;
+    }
+
 }
