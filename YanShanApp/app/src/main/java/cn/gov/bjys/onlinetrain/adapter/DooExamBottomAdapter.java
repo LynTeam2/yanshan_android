87933b4cf247fc24172fb80d7c0c42ae99438263
zhoudodo <635547767@qq.com>
2017-10-20 14:12:44 +0800

    1

--- YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/adapter/DooExamBottomAdapter.java
+++ YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/adapter/DooExamBottomAdapter.java
@@ -3,8 +3,31 @@ package cn.gov.bjys.onlinetrain.adapter;
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 
+import java.util.List;
+
+import cn.gov.bjys.onlinetrain.bean.ExamXqBean;
+
 /**
  * Created by dodozhou on 2017/10/20.
  */
-public class DooExamBottomAdapter extends BaseMultiItemQuickAdapter< ,BaseViewHolder> {
-}/
+public class DooExamBottomAdapter extends BaseMultiItemQuickAdapter<ExamXqBean,BaseViewHolder> {
+
+    /**
+     * Same as QuickAdapter#QuickAdapter(Context,int) but with
+     * some initialization data.
+     *
+     * @param data A new list is created out of this one to avoid mutable list
+     */
+    public DooExamBottomAdapter(List<ExamXqBean> data) {
+        super(data);
+        addItemType(ExamXqBean.NOMAL,);
+        addItemType(ExamXqBean.CHOICE,);
+        addItemType(ExamXqBean.RIGHT,);
+        addItemType(ExamXqBean.FAIL,);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, ExamXqBean item) {
+
+    }
+}
--- YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/bean/ExamXqBean.java
+++ YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/bean/ExamXqBean.java
@@ -1,12 +1,47 @@
 package cn.gov.bjys.onlinetrain.bean;
 
 
+import com.chad.library.adapter.base.entity.MultiItemEntity;
 
 //维护题目列表的展示bean 记录题目的完成情况
-public class ExamXqBean {
+public class ExamXqBean implements MultiItemEntity {
 
     public final static int NOMAL = 0;//未选择
     public final static int CHOICE = 1;//目前选择状态
     public final static int RIGHT = 2;//题目对的状态
     public final static int FAIL = 3;//题目错误的状态
+
+
+    private int mType = NOMAL;
+    private int mPosition = 0;//位置position也是内容
+    private int mSpanSize = 1;//
+
+    public int getmType() {
+        return mType;
+    }
+
+    public void setmType(int mType) {
+        this.mType = mType;
+    }
+
+    public int getmPosition() {
+        return mPosition;
+    }
+
+    public void setmPosition(int mPosition) {
+        this.mPosition = mPosition;
+    }
+
+    public int getmSpanSize() {
+        return mSpanSize;
+    }
+
+    public void setmSpanSize(int mSpanSize) {
+        this.mSpanSize = mSpanSize;
+    }
+
+    @Override
+    public int getItemType() {
+        return mType;
+    }
 }
