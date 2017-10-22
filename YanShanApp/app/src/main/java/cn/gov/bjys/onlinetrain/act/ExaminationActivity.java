f9e233e92a2dc00ab5596f6955682c784c7b1fb2
zhoudodo <635547767@qq.com>
2017-10-22 22:30:26 +0800

    add

--- YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/act/ExaminationActivity.java
+++ YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/act/ExaminationActivity.java
@@ -2,6 +2,7 @@ package cn.gov.bjys.onlinetrain.act;
 
 import android.animation.ObjectAnimator;
 import android.support.v4.view.ViewPager;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -75,15 +76,42 @@ public class ExaminationActivity extends FrameActivity {
     public void initViews() {
         super.initViews();
 //        prepareAnimator();
+
+        mGestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                // if (Math.abs(e1.getRawX() - e2.getRawX()) > 250) {
+                // // System.out.println("水平方向移动距离过大");
+                // return true;
+                // }
+                if (Math.abs(velocityY) < 100) {
+                    // System.out.println("手指移动的太慢了");
+                    return true;
+                }
+
+                // 手势向下 down
+                if ((e2.getRawY() - e1.getRawY()) > 200) {
+                    repeatBottomAnimator();//在此处控制关闭
+                    return true;
+                }
+                // 手势向上 up
+                if ((e1.getRawY() - e2.getRawY()) > 200) {
+                    startBottomAnimator();
+                    return true;
+                }
+                return super.onFling(e1, e2, velocityX, velocityY);
+            }
+        });
+
         mExamBottomLayout.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
+                mGestureDetector.onTouchEvent(event);
                 return false;
             }
         });
     }
-
+    private GestureDetector mGestureDetector;
     private void prepareAnimator(){
 
         float translationsY  = mExamBottomLayout.getTranslationY();
