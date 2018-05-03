1eecb26e7c76e819ae41db3d03f1a67303ed36a5
zhoudodo <635547767@qq.com>
2018-05-03 18:34:54 +0800

    add bean

---
+++ YanShanApp/app/src/main/java/cn/gov/bjys/onlinetrain/bean/AvatarBackBean.java
@@ -0,0 +1,26 @@
+package cn.gov.bjys.onlinetrain.bean;
+
+/**
+ * Created by dodo on 2018/5/3.
+ */
+
+public class AvatarBackBean {
+    private String name;
+    private String path;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
